b9328a7ed895a0dcc6c3ae4ae41cf241cc2f14ae
Morley Zhi <morley@springtactics.com>
2019-03-20 16:44:44 -0400

    Dev environment (#5)
    * Add Jest
    * Add scripts for building, linting, and testing the codebase
    * Implement two stateless functions
    * Separate stateful, Stellar network-connected functions into a class, DataProvider; more on this to come later

---
+++ src/data.ts
@@ -0,0 +1,71 @@
+import { Account, Effect, ReframedEffect, Token } from "./types";
+
+export * from "./DataProvider";
+
+/**
+ * Get the string identifier for a token.
+ * @returns "native" if the token is native, otherwise returns `${tokenCode}:${issuerKey}`.
+ */
+export function getTokenIdentifier(token: Token): string {
+  if (token.type === "native" || !token.issuer) {
+    return "native";
+  }
+
+  return `${token.code}:${token.issuer.key}`;
+}
+
+function isReframedEffect(obj: any): obj is ReframedEffect {
+  return obj.baseToken !== undefined;
+}
+
+/**
+ * Reframe a normal effect from a specific observer account's
+ * perspective.
+ */
+export function reframeEffect(
+  observerAccount: Account,
+  effect: Effect | ReframedEffect,
+): ReframedEffect {
+  // if the effect has already been reframed, we're done!
+  if (isReframedEffect(effect)) {
+    return effect;
+  }
+
+  const {
+    id,
+    senderToken,
+    receiverToken,
+    senderAccount,
+    receiverAccount,
+    senderAmount,
+    receiverAmount,
+    timestamp,
+    type,
+  } = effect;
+
+  const isObserverSender = senderAccount.publicKey === observerAccount.publicKey;
+
+  if (isObserverSender) {
+    return {
+      id,
+      baseToken: senderToken,
+      token: receiverToken,
+      amount: receiverAmount,
+      price: senderAmount,
+      sender: receiverAccount,
+      timestamp,
+      type,
+    };
+  }
+
+  return {
+    id,
+    baseToken: receiverToken,
+    token: senderToken,
+    amount: senderAmount,
+    price: receiverAmount,
+    sender: senderAccount,
+    timestamp,
+    type,
+  };
+}
---
+++ src/types.ts
@@ -0,0 +1,70 @@
+import BigNumber from "bignumber.js";
+
+export enum TokenType {
+  native = "native",
+  credit_alphanum4 = "credit_alphanum4",
+  credit_alphanum12 = "credit_alphanum12",
+}
+
+export enum EffectType {
+  account_created = "account_created",
+  account_credited = "account_credited",
+  account_debited = "account_debited",
+  account_home_domain_updated = "account_home_domain_updated",
+  account_inflation_destination_updated = "account_inflation_destination_updated",
+  account_removed = "account_removed",
+  account_thresholds_updated = "account_thresholds_updated",
+  signer_created = "signer_created",
+  signer_removed = "signer_removed",
+  signer_updated = "signer_updated",
+  trade = "trade",
+  trustline_created = "trustline_created",
+  trustline_removed = "trustline_removed",
+  trustline_updated = "trustline_updated",
+}
+
+export interface Account {
+  publicKey: string;
+}
+
+export interface Issuer {
+  key: string;
+  name: string;
+  url: string;
+  hostName: string;
+}
+
+export interface Token {
+  type: TokenType;
+  code: string;
+  issuer?: Issuer;
+  anchorAsset?: string;
+  numAccounts?: BigNumber;
+  amount?: BigNumber;
+  bidCount?: number;
+  askCount?: number;
+  spread?: BigNumber;
+}
+
+export interface Effect {
+  id: string;
+  type: EffectType;
+  senderToken: Token;
+  receiverToken: Token;
+  senderAccount: Account;
+  receiverAccount: Account;
+  senderAmount: BigNumber;
+  receiverAmount: BigNumber;
+  timestamp: number;
+}
+
+export interface ReframedEffect {
+  id: string;
+  type: EffectType;
+  baseToken: Token;
+  token: Token;
+  amount: BigNumber;
+  price: BigNumber;
+  sender: Account;
+  timestamp: number;
+}
