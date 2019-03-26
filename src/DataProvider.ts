e80a2bf5b7e816bfdee8631f8ea3f6943028b99c
Morley Zhi <morley@stellar.org>
2019-03-26 11:48:42 -0400

    first draft of watcher. using setInterval is kind of nasty.

--- src/DataProvider.ts
+++ src/DataProvider.ts
@@ -1,28 +1,52 @@
 import { Server } from "stellar-sdk";
 
 import { makeDisplayableBalances } from "./makeDisplayableBalances";
-import { Account } from "./types";
+import { Account, Balances } from "./types";
 
 function isAccount(obj: any): obj is Account {
   return obj.publicKey !== undefined;
 }
 
+interface DataProviderParams {
+  serverUrl: string;
+  accountOrKey: Account | string;
+}
+
+interface BalanceWatcherParams {
+  onMessage: (balances: Balances) => void;
+  onError: (error: any) => void;
+}
+
 export class DataProvider {
   private server: Server;
+  private accountKey: string;
 
-  constructor(serverUrl: string) {
-    this.server = new Server(serverUrl);
+  constructor(params: DataProviderParams) {
+    this.server = new Server(params.serverUrl);
+    this.accountKey = isAccount(params.accountOrKey)
+      ? params.accountOrKey.publicKey
+      : params.accountOrKey;
   }
 
-  public async getBalancesForAccount(accountOrKey: Account | string) {
-    const accountKey = isAccount(accountOrKey)
-      ? accountOrKey.publicKey
-      : accountOrKey;
+  public async getBalances() {
     const accountSummary = await this.server
       .accounts()
-      .accountId(accountKey)
+      .accountId(this.accountKey)
       .call();
 
     return makeDisplayableBalances(accountSummary);
+  }
+
+  public watchBalances(params: BalanceWatcherParams) {
+    const { onMessage, onError } = params;
+    let timer: any;
+
+    timer = setInterval(() => {
+      this.getBalances()
+        .then(onMessage)
+        .catch(onError);
+    }, 3000) as any;
+
+    return () => clearTimeout(timer);
   }
 }
