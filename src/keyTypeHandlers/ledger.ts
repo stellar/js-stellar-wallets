1d100e2907b0884a35745c016a504ece014ce061
Morley Zhi <morley@stellar.org>
2019-04-09 18:43:10 -0400

    naive ledger support

--- src/keyTypeHandlers/ledger.ts
+++ src/keyTypeHandlers/ledger.ts
@@ -1,11 +1,14 @@
+import LedgerTransport from "@ledgerhq/hq-transport-u2f";
+import LedgerStr from "@ledgerhq/hw-app-str";
 import { Transaction } from "stellar-base";
+import StellarSdk from "stellar-sdk";
 
 import { isLedgerKey } from "../KeyHelpers";
 import { Key, KeyType, KeyTypeHandler } from "../types";
 
 export const ledgerHandler: KeyTypeHandler = {
   keyType: KeyType.ledger,
-  signTransaction({
+  async signTransaction({
     transaction,
     key,
   }: {
@@ -19,6 +22,27 @@ export const ledgerHandler: KeyTypeHandler = {
         )}`,
       );
     }
+
+    /* 
+      There's a naive way to do this (to keep all functions stateless and 
+      make the connection anew each time), and there's some way of weaving state
+      into this.
+
+      Gonna do the naive thing first and then figure out how to do this right.
+    */
+    const transport = await LedgerTransport.create(60 * 1000);
+    const ledgerApi = new LedgerStr(transport);
+    const result = await ledgerApi.signTransaction(
+      "44'/148'/0'",
+      transaction.signatureBase(),
+    );
+
+    const keyPair = StellarSdk.Keypair.fromPublicKey(key.publicKey);
+    const decoratedSignature = new StellarSdk.xdr.DecoratedSignature({
+      hint: keyPair.signatureHint(),
+      signature: result.signature,
+    });
+    transaction.signatures.push(decoratedSignature);
 
     console.log("key: ", key);
     return Promise.resolve(transaction);
