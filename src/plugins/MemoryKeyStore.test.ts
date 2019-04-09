11b6f3d94f361b00656dcf6cea538cfc93814d50
Morley Zhi <morley@stellar.org>
2019-04-09 12:08:18 -0400

    test removing keys

--- src/plugins/MemoryKeyStore.test.ts
+++ src/plugins/MemoryKeyStore.test.ts
@@ -44,4 +44,39 @@ describe("MemoryKeyStore", function() {
 
     expect(allKeys).toEqual([encryptedKey]);
   });
+
+  it("properly deletes keys", async () => {
+    const testStore = new MemoryKeyStore();
+
+    const encryptedKey: EncryptedKey = {
+      key: {
+        type: "Angel",
+        publicKey: "AVACYN",
+      },
+      encrypterName: "Test",
+      salt: "SLFKJSDLKFJLSKDJFLKSJD",
+    };
+
+    const keyMetadata = {
+      type: "Angel",
+      encrypterName: "Test",
+      publicKey: "AVACYN",
+      creationTime: 666,
+      modifiedTime: 666,
+    };
+
+    await testStore.storeKeys([encryptedKey]);
+
+    const allKeys = await testStore.loadAllKeys();
+
+    expect(allKeys).toEqual([encryptedKey]);
+
+    const removalMetadata = await testStore.removeKey("AVACYN");
+
+    expect(removalMetadata).toEqual(keyMetadata);
+
+    const noKeys = await testStore.loadAllKeys();
+
+    expect(noKeys).toEqual([]);
+  });
 });
