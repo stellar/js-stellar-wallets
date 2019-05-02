90019b3433d46b3a3aa97e0641b313afe6fc7dce
Howard Chen <howard@chain.com>
2019-05-02 14:37:52 -0400

    Revert "try catch"
    This reverts commit 29edcb49a89ed33f4ea479ff90438995b758e884.

--- src/KeyManager.ts
+++ src/KeyManager.ts
@@ -93,12 +93,7 @@ export class KeyManager {
     // happy path-only code to demonstrate idea
     const encrypterObj = this.encrypterMap[encrypterName];
     const encryptedKey = await encrypterObj.encryptKey({ key, password });
-    let keyMetadata;
-    try {
-      keyMetadata = await this.keyStore.storeKeys([encryptedKey]);
-    } catch (e) {
-      throw e;
-    }
+    const keyMetadata = await this.keyStore.storeKeys([encryptedKey]);
 
     this._writeIndexache(key.publicKey, key);
 
