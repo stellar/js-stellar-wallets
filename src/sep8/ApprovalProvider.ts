059ff458ad6d880aee37558c42e71af4978cf066
Howard Chen <howard@stellar.org>
2021-02-04 20:07:34 -0500

    add approve func

---
+++ src/sep8/ApprovalProvider.ts
@@ -0,0 +1,77 @@
+import { Transaction } from "stellar-base";
+import { ApprovalResponse } from "../types/sep8";
+
+export class ApprovalProvider {
+  public approvalServer: string;
+
+  constructor(approvalServer: string) {
+    if (!approvalServer) {
+      throw new Error("Required parameter `approvalServer` missing!");
+    }
+
+    this.approvalServer = approvalServer.replace(/\/$/, "");
+  }
+
+  public async approve(
+    transaction: Transaction,
+    contentType:
+      | "application/json"
+      | "application/x-www-form-urlencoded" = "application/json",
+  ): Promise<ApprovalResponse> {
+    const param = {
+      tx: transaction
+        .toEnvelope()
+        .toXDR()
+        .toString("base64"),
+    };
+
+    let response;
+    if (contentType === "application/json") {
+      response = await fetch(this.approvalServer, {
+        method: "POST",
+        body: JSON.stringify(param),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } else {
+      const urlParam = new URLSearchParams(param);
+      response = await fetch(this.approvalServer, {
+        method: "POST",
+        body: urlParam.toString(),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+    }
+
+    if (!response.ok) {
+      const responseText = await response.text();
+      try {
+        const { responseJson } = JSON.parse(responseText);
+        throw new Error(
+          `Error sending base64-encoded transaction to ${
+            this.approvalServer
+          }: error ${responseJson.error}`,
+        );
+      } catch (e) {
+        throw new Error(
+          `Error sending base64-encoded transaction to ${
+            this.approvalServer
+          }: error 
+          code ${response.status}, status text: "${responseText}"`,
+        );
+      }
+    }
+
+    const responseOKText = await response.text();
+
+    try {
+      return JSON.parse(responseOKText) as ApprovalResponse;
+    } catch (e) {
+      throw new Error(
+        `Error parsing the approval server response as JSON: ${responseOKText}`,
+      );
+    }
+  }
+}
